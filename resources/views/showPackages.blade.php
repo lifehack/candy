3bef2d81377bd2237b4654492205db2da628dc47
SummerRainC1030 <summerrainc1030@gmail.com>
2016-04-27 21:42:11 +0800

    更改外观，添加假导航bar

--- resources/views/showPackages.blade.php
+++ resources/views/showPackages.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('content')
 
-    <div style="border-right-width: 0px;" class="row text-center">
+    <div style="border-right-width:0px; padding-left: 6%"  class="row text-center">
         <h1 style="color:white" class="text-center">预约录音</h1>
         <h3 style="color:white;font-size: medium;width:90%" class="text-left">&nbsp&nbsp&nbsp&nbsp选择店面：</h3>
         <table class="a1" cellspacing="1" cellpadding="0" width=94%"
@@ -16,10 +16,10 @@
                 <td>&nbsp&nbsp</td>
             </tr>
         </table>
-        <table class="a1" cellspacing="1" cellpadding="0" width="94%"
+        <table class="a1" cellspacing="0" cellpadding="0" width="94%"
                style="color:white;background-color:black;font-size:smaller;height:35px" align="center" border="0">
             <td><div class="row">
-                <img width="94%" height="15%" id="shop_image" src="img/shop1.jpg"> </img>
+                <img style="padding-left: 6%;padding-bottom: 3%" width="94%" height="15%" id="shop_image" src="img/shop1.jpg"> </img>
             </div>
             </td>
         </table>
@@ -31,24 +31,23 @@
             <div style="background-color: white" class="col-md-offset-4 col-lg-offset-5 col-md-2 col-lg-1">
                 <div id="calendar"></div>
             </div>
-            <div style="background-color: white" class="col-md-offset-1 col-lg-offset-1 col-md-2 col-lg-1">
+            <div style="background-color: white;" class="col-md-offset-1 col-lg-offset-1 col-md-1 col-lg-1">
                 <div class="panel panel-primary">
                     <div class="panel-heading" id="daySelect" style="background-color: black">
                         点击日期
                     </div>
-                    <div class="panel-body">
-                        <p id="dayTimes"></p>
-
+                    <div  align="center" class="panel-body">
+                        <p  id="dayTimes"></p>
                     </div>
                     <div class="panel panel-primary">
-                        <div class="panel-heading" id="daySelect" style="background-color: black">
-                            <p value="id"></p>
-                            <a id="shop_phone" href="">点击拨打预约电话</a>
+                        <div class="panel-heading" id="daySelect" style="background-color: black;padding-bottom: 3%">
+                            <a style="color: white" id="shop_phone" href="">点击拨打预约咨询电话</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <p style="height: 1%">&nbsp;</p>
     </div>
     <!-- Calendar Functionality -->
     <script src="{{ asset('/js/calendar.js') }}"></script>
