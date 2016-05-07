8e3ca430c5c255eff861d84c56ba51608c20ae30
SummerRainC1030 <summerrainc1030@gmail.com>
2016-05-07 23:19:39 +0800

    参考添加了Socialite http://socialiteproviders.github.io/providers/weixin/

--- config/app.php
+++ config/app.php
@@ -113,6 +113,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+	SocialiteProviders\Manager\ServiceProvider::class, // add
         Illuminate\Foundation\Providers\ArtisanServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
@@ -188,6 +189,7 @@ return [
         'Route'     => Illuminate\Support\Facades\Route::class,
         'Schema'    => Illuminate\Support\Facades\Schema::class,
         'Session'   => Illuminate\Support\Facades\Session::class,
+	'Socialite' => Laravel\Socialite\Facades\Socialite::class, //add
         'Storage'   => Illuminate\Support\Facades\Storage::class,
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
